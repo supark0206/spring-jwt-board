885fd2ed96da886b4c776299bc238b055766c6f6
supark0206 <supark0206@gmail.com>
2023-08-07 12:31:36 +0900

    Initial commit

---
+++ src/test/java/wanted/preassignment/PreAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package wanted.preassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
